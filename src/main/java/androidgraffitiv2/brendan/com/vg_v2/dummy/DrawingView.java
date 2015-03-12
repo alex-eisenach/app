fd811304d7833070e057c21086e43606a9d736fa
alex-eisenach <alex.eisenach303@gmail.com>
2015-03-11 23:26:16 -0600

    addd DrawingView.java

---
+++ src/main/java/androidgraffitiv2/brendan/com/vg_v2/dummy/DrawingView.java
@@ -0,0 +1,22 @@
+package androidgraffitiv2.brendan.com.vg_v2.dummy;
+
+import android.view.View;
+import android.content.Context;
+import android.util.AttributeSet;
+
+
+/**
+ * Created by Alex Eisenach on 3/11/2015.
+ */
+public class DrawingView extends View {
+
+    public DrawingView(Context context, AttributeSet attrs){
+        super(context, attrs);
+        setupDrawing();
+    }
+
+    private void setupDrawing(){
+
+    }
+
+}
